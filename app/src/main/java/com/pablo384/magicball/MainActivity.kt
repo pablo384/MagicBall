cdba3c2e5f896f90af98666387d26e6097f23316
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2017-08-21 22:34:23 -0400

    generando color aleatoriamente

--- app/src/main/java/com/pablo384/magicball/MainActivity.kt
+++ app/src/main/java/com/pablo384/magicball/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.pablo384.magicball
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //1- clic listener
+        //2- generar un color aleatorio
+        //3- generar una frase aleatoria
+
+        pressMeButton.setOnClickListener {
+            val randomColor= getRandomColor()
+        }
+    }
+
+    private fun getRandomColor(): Int {
+        val colors = intArrayOf(Color.CYAN, Color.RED, Color.MAGENTA, Color.GREEN, Color.BLACK)
+        val random = (Math.random()*colors.size).toInt()
+        return colors[random]
     }
 }
